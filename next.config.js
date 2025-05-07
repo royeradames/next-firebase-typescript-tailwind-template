8626edecdd62bb6313a6d637f28d8f1636dc8b37
Royer Adames <royeraadames@gmail.com>
2025-05-07 15:02:33 -0400

    Display authenticated user data in the sign in only dashboard (#5)
    * Add firebase image domain configuration to next.config.js
    
    * Refactor Content component structure
    
    - Changed the main wrapper from a <div> to a <section> for better semantic HTML.
    - Updated the title element from a <div> to an <h1> to improve accessibility and SEO.
    
    * Display sign in data after user is sign in

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    domains: ['lh3.googleusercontent.com'],
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
