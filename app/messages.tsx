efd38ac9c3c58d717593b9114a359abcfed34d0f
Royer Adames <royeraadames@gmail.com>
2025-05-07 09:26:07 -0400

    migrate pages/messages to app/messages

---
+++ app/messages.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Shell from "../components/shell";
+import Content from "../components/content/Content";
+import { userIsLoggedIn } from "../firebase/auth/utils";
+import { redirect } from "next/navigation";
+import { cookies } from "next/headers";
+
+export default async function Messages() {
+  const cookiesStore = cookies();
+  const authenticated = await userIsLoggedIn(cookiesStore);
+
+  if (!authenticated) {
+    redirect('/login')
+  }
+  
+  return (
+    <Shell>
+      <Content title="Messages">
+        <>Messages</>
+      </Content>
+    </Shell>
+  );
+}
+
