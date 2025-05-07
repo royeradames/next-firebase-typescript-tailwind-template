aa814dd892398784f09b46096abb6a08059e58e8
Royer Adames <royeraadames@gmail.com>
2025-05-07 15:00:39 -0400

    Refactor Content component structure
    - Changed the main wrapper from a <div> to a <section> for better semantic HTML.
    - Updated the title element from a <div> to an <h1> to improve accessibility and SEO.

--- components/content/Content.tsx
+++ components/content/Content.tsx
@@ -7,11 +7,11 @@ type Props = {
 
 export default function Content({ title, children }: Props) {
   return (
-    <div className="flex flex-col">
-      <div className="text-xl font-bold text-gray-600 border-b-2 border-blue-200 pt-6 pb-2 px-6">
+    <section className="flex flex-col">
+      <h1 className="text-xl font-bold text-gray-600 border-b-2 border-blue-200 pt-6 pb-2 px-6">
         {title}
-      </div>
+      </h1>
       <div className="flex-1 my-6 mx-6 rounded-xl">{children}</div>
-    </div>
+    </section>
   );
 }
